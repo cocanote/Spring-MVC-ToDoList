bd98b284be211a2ae913bef38a9ac7325c89cd53
Junsu <weekress027@gmail.com>
2020-07-17 00:24:55 +0900

    feat : make new interface ITodoService and class TodoService

--- spring-mvc-todo/src/main/java/comm/spp/todolist/service/ITodoService.java
+++ spring-mvc-todo/src/main/java/comm/spp/todolist/service/ITodoService.java
@@ -1,5 +1,13 @@
 package comm.spp.todolist.service;
 
+import java.util.ArrayList;
+
+import com.spp.todolist.dto.Todo;
+
+
+
 public interface ITodoService {
 
+	void registerTodo(String thing, String name, String priority, String id) ;
+	ArrayList<Todo> readTodos();
 }
--- spring-mvc-todo/src/main/java/comm/spp/todolist/service/TodoService.java
+++ spring-mvc-todo/src/main/java/comm/spp/todolist/service/TodoService.java
@@ -1,5 +1,32 @@
 package comm.spp.todolist.service;
 
-public class TodoService {
+import java.util.ArrayList;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.spp.todolist.dao.TodoDao;
+import com.spp.todolist.dto.Todo;
+
+public class TodoService implements ITodoService{
+
+	TodoDao todoDao;
+	
+	@Autowired
+	public void setTodoDao(TodoDao todoDao) {
+		this.todoDao = todoDao;
+	}
+
+	@Override
+	public void registerTodo(String thing, String name, String priority, String id) {
+		
+		
+		
+	}
+
+	@Override
+	public ArrayList<Todo> readTodos() {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
